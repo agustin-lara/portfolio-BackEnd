31fe48ff6ee17d7e1bcac77e37f19e7952906094
Agustin Lara <lara.agustin.c@gmail.com>
2023-04-18 19:50:58 -0300

    Implementar funcionalidades a Persona: - Especificar columnas del Modelo y agregar Constructor - Especificar CORS para utilizar la API localmente

--- src/main/java/com/agustinlaradesarrollador/portfolioBackEnd/Controller/PersonaController.java
+++ src/main/java/com/agustinlaradesarrollador/portfolioBackEnd/Controller/PersonaController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/personas")
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     
     @Autowired
@@ -61,7 +63,7 @@ public class PersonaController {
             persona.setImagen_perfil(nuevaPersona.getImagen_perfil());
             persona.setImagen_portada(nuevaPersona.getImagen_portada());
             iPersonaService.savePersona(persona);
-            return new ResponseEntity("Persona actualizada.",HttpStatus.OK);
+            return new ResponseEntity(persona,HttpStatus.OK);
         }
     }
     
--- src/main/java/com/agustinlaradesarrollador/portfolioBackEnd/Model/Persona.java
+++ src/main/java/com/agustinlaradesarrollador/portfolioBackEnd/Model/Persona.java
@@ -1,5 +1,6 @@
 package com.agustinlaradesarrollador.portfolioBackEnd.Model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,10 +20,31 @@ public class Persona {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
+    
+    @Column(name = "nombre")
     private String nombre;
+    
+    @Column(name = "titulo")
     private String titulo;
+    
+    @Column(name = "descripcion", length = 512)
     private String descripcion;
+    
+    @Column(name = "imagen_perfil")
     private String imagen_perfil;
+    
+    @Column(name = "imagen_portada")
     private String imagen_portada;
+    
+    public Persona(String nombre, String titulo, String descripcion, String imagen_perfil, String imagen_portada) {
+        this.nombre = nombre;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.imagen_perfil = imagen_perfil;
+        this.imagen_portada = imagen_portada;
+    }
+    
+    public Persona() {
+    }
     
 }
